430bc444991212d7b19fcdc5522016620c2fbc51
Harrison Fung <harrison.hok.fung@gmail.com>
2019-02-25 09:30:55 -0500

    added styling to the calculator and added input

--- react-calculator/src/components/Calculator.js
+++ react-calculator/src/components/Calculator.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import Button from "./Button";
-import { Row, Container} from 'react-bootstrap';
+import { Row, Container,InputGroup, FormControl} from 'react-bootstrap';
 
 const Calculator = () => {
   return (
     <div>
-      <Container fluid={true}>
+      <Container className="container" fluid={true}>
+        <Row noGutters={true}>
+        <InputGroup className="input">
+          <FormControl>
+          </FormControl>
+        </InputGroup>
+        </Row>
         <Row noGutters={true}>
         <Button number = {1} />
         <Button number = {2} />
         <Button number = {3} />
+        <Button number = {"+"} />
         </Row>
         <Row noGutters={true}>
         <Button number = {4} />
         <Button number = {5} />
         <Button number = {6} />
+        <Button number = {"-"} />
         </Row>
         <Row noGutters={true}>
         <Button number = {7} />
         <Button number = {8} />
         <Button number = {9} />
+        <Button number = {"*"} />
+        </Row>
+        <Row noGutters={true}>
+        <Button number = {0} />
+        <Button number = {"="} />
+        <Button number = {"C"} />
+        <Button number = {"/"} />
         </Row>
         </Container>
+        
     </div>
   )
 }
