32b0ba1adebcfc40372a1fe8ae1beec96a6fbd62
Harrison Fung <harrison.hok.fung@gmail.com>
2021-06-28 14:11:33 -0400

    Updates

---
+++ precache-manifest.3ccb8010ca33220e0c68c835ed9e22a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7517f991f963875c35ff",
+    "url": "/react-calculator/static/js/main.7517f991.chunk.js"
+  },
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.6473c924.js"
+  },
+  {
+    "revision": "dde2c6f69813ab5b4ffb",
+    "url": "/react-calculator/static/js/2.dde2c6f6.chunk.js"
+  },
+  {
+    "revision": "288b4da11f81be591d6fb43d99670ad7",
+    "url": "/react-calculator/index.html"
+  }
+];
